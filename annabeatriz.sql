2029932857f274110c8fd8264828a8cd0c2bdd11
Anna Beatriz <annavalentim.tds1.senai@gmail.com>
2024-11-05 09:24:26 -0300

    create table reservas

--- annabeatriz.sql
+++ annabeatriz.sql
@@ -12,3 +12,15 @@ CREATE TABLE quartos(
     id_quarto SERIAL PRIMARY KEY,
     numero DECIMAL(3,0) UNIQUE NOT NULL
 );
+
+CREATE TABLE reservas(
+    id_reserva SERIAL PRIMARY KEY,
+    data_inicio_reserva DATE NOT NULL,
+    hora_reserva TIME NOT NULL,
+    data_termino_reserva DATE NOT NULL,
+    id_hospede INT NOT NULL,
+    id_quarto INT NOT NULL,
+    CONSTRAINT fk_hospede FOREIGN KEY (id_hospede) REFERENCES hospedes(id_hospede),
+    CONSTRAINT fk_quarto FOREIGN KEY (id_quarto) REFERENCES quartos(id_quarto)
+);
+
